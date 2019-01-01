c6a56227cea957c91777b46e8c29055f69808473
Yussin R. Hidayat <van.de.bugs@gmail.com>
2019-01-01 19:12:00 +0800

    Add initial Activity page.

--- src/controller/ActivityController.php
+++ src/controller/ActivityController.php
@@ -19,7 +19,23 @@ class ActivityController {
      */
     public function view($id) {
         $template = new Template('/activities/detail.php');
-        $template->setParameter('id', $id);
+        
+        $activity = new Activity();
+        $activity->setId($id);
+        $activity->setTitle('Title ' . $id);
+        $activity->setContent('<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rhoncus aliquam felis vel accumsan. Aliquam at nisl et magna ornare faucibus. Nulla neque leo, pharetra quis felis vitae, mattis consequat erat. Sed suscipit, dolor ut pretium aliquam, elit tortor egestas tellus, eu volutpat magna augue eu turpis. Integer at quam odio. Mauris lacinia nisi at cursus gravida. In mattis pellentesque neque, at feugiat mauris blandit eget.</p>' .
+            '<p>In tincidunt, magna at volutpat tincidunt, mauris libero laoreet mi, a tempor lectus augue at nibh. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus mollis mi accumsan tortor laoreet, ac aliquet dui sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla vitae dui sit amet magna dignissim congue id eu urna. Curabitur nibh mauris, porta nec ipsum in, molestie rhoncus lectus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In quis arcu leo. Mauris luctus efficitur ligula vel vestibulum. Nullam elementum libero purus, at malesuada velit fringilla eu. Vestibulum bibendum porta auctor.</p>' .
+            '<p>Aenean a fringilla massa. Vestibulum pretium cursus purus, ut euismod ante fringilla vel. Curabitur congue diam elementum, maximus ante vel, luctus tortor. Praesent tristique leo non ligula molestie aliquet. Vestibulum sed mauris enim. Duis in tellus pretium, viverra diam in, semper tellus. Donec rutrum cursus nunc eu fringilla. Nullam bibendum facilisis elit ut dictum.</p>' .
+            '<p>Donec at nulla cursus, posuere diam eget, auctor quam. Ut non enim sed nibh ullamcorper imperdiet auctor ut magna. Aliquam erat volutpat. Donec at molestie eros, id gravida diam. Morbi lectus dolor, mollis a consectetur eu, interdum quis ligula. Cras laoreet magna a neque vehicula aliquet. Suspendisse cursus tincidunt imperdiet. Nunc feugiat tortor diam, at tincidunt tortor lacinia quis. Pellentesque quis eros rhoncus, mollis erat id, tempor ante. Aliquam ac imperdiet sapien. Pellentesque vulputate blandit ex ac laoreet. Etiam convallis, nunc vitae tincidunt dignissim, nisi sapien semper orci, sed luctus velit arcu in dolor. Proin eget augue nibh. Nam felis justo, tristique quis velit sed, vehicula blandit metus. Nullam non ipsum ultricies enim sodales condimentum.</p>' .
+            '<p>Nulla in elit nec quam laoreet efficitur. Suspendisse auctor, dolor vel porttitor iaculis, risus arcu scelerisque nulla, vitae blandit turpis ex vel ante. Curabitur tempus quis urna et interdum. Nullam posuere sem urna, non vulputate leo rutrum non. Aliquam placerat nulla in tortor pharetra, eu rutrum lectus posuere. Aliquam magna metus, molestie ac pharetra sed, faucibus eget urna. Sed suscipit, velit sit amet sagittis sagittis, odio enim lacinia lectus, vel lobortis tortor odio ac dolor. Maecenas eget augue lobortis, consequat ante a, commodo quam. Ut ultricies odio et bibendum euismod. Suspendisse id elit sagittis dui scelerisque posuere. Ut ut arcu non tortor commodo ultricies. Maecenas vel facilisis neque. Sed vulputate mauris ipsum, in scelerisque quam tempus id. Quisque vestibulum nibh id odio pretium gravida. Morbi nec augue quis urna tempus malesuada ut sed sapien. Praesent tristique mollis ante, a faucibus dolor feugiat sit amet.</p>');
+        $activity->setCreatorId('Admin');
+        $activity->setCreationDate('19 Agustus 2018');
+
+        $category = new Category();
+        $category->setName('Any');
+        $category->setIconName('far fa-bookmark');
+        $activity->setCategory($category);
+        $template->setParameter('activity', $activity);
         
         return $template;
     }
@@ -62,7 +78,7 @@ class ActivityController {
             $activity->setTitle('Title ' . $i);
             $activity->setContent('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.');
             $activity->setCreatorId('Admin');
-            $activity->setCreationDate('19/08/2018');
+            $activity->setCreationDate('19 Agustus 2018');
             
             $category = new Category();
             $category->setName('Any');
--- web/activities/detail.php
+++ web/activities/detail.php
@@ -1,98 +1,49 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>ACTIVITIES</title>
-        <?php $f->includeJS('/assets/js/jquery-3.3.1.min.js') ?>
-    </head>
-    <body>
-        <p>This is ACTIVITY page</p>
-        <form id="form">
-            <input type="hidden" name="id" value="<?php echo $id ?>" />
-            <table>
-                <tr>
-                    <td>ID</td>
-                    <td><?php echo $id ?></td>
-                </tr>
-                <tr>
-                    <td>Title</td>
-                    <td><input id="title" name="title"><label class="error" for="title"></label></td>
-                </tr>
-                <tr>
-                    <td>Content</td>
-                    <td><textarea id="content" name="content"></textarea><label class="error" for="content"></label></td>
-                </tr>
-                <tr>
-                    <td colspan="2">
-                        <button id="button_delete" type="button">Delete</button>
-                        <button>Save</button>
-                    </td>
-                </tr>
-            </table>
-        </form>
-        <p>Click <a href="<?php echo $f->url('/') ?>">here</a> to go to HOME page.</p>
-        <p>Click <a href="<?php echo $f->url('/activities') ?>">here</a> to go to ACTIVITIES page.</p>
-    </body>
-    <style>
-        .error {
-            display: block;
-            color: red;
-        }
-    </style>
-    <script>
-        $(function() {
-            $.getJSON(
-                '<?php echo $f->url('/api/activities/' . $id) ?>'
-            ).done(function(response) {
-                var data = response.data;
-                var titleField = $('#title');
-                titleField.val(data.title);
+<html lang="en">
 
-                var contentField = $('#content');
-                contentField.val(data.content);
-            });
-            
-            $('#form').submit(function(event) {
-                event.preventDefault();
-                
-                var data = {};
-                var form = $(this);
-                form.find('[name]').each(function() {
-                    var item = $(this);
-                    data[item.attr('name')] = item.val();
-                });
-                form.find('label').text(null);
-                
-                $.ajax({
-                    url: '<?php echo $f->url('/api/activities/' . $id) ?>',
-                    type: 'PUT',
-                    data: JSON.stringify(data),
-                    contentType: 'application/json'
-                }).done(function(response) {
-                    if (!response.code) {
-                        alert('Success!');
-                    } else if (response.data) {
-                        $.each(response.data, function(key, value) {
-                            var label = form.find('label[for=' + key + ']');
-                            label.text(value);
-                        });
-                    } else {
-                        alert('Error: ' + response.message);
-                    }
-                });
-            });
-            
-            $('#button_delete').click(function() {
-                $.ajax({
-                    url: '<?php echo $f->url('/api/activities/' . $id) ?>',
-                    type: 'DELETE'
-                }).done(function(response) {
-                    if (!response.code) {
-                        alert('Success!');
-                    } else {
-                        alert('Error: ' + response.message);
-                    }
-                });
-            });
-        });
-    </script>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Rumah Santri Indonesia - Program Santri Asuh | <?php echo $activity->getTitle() ?></title>
+        <?php include('./web/template/links.php'); ?>
+    </head>
+
+    <body class="d-flex flex-column grey lighten-3">
+        <header>
+            <?php include('./web/template/navigation.php'); ?>
+            <nav aria-label="Breadcrumb" class="breadcrumb-container">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"></li>
+                    <li class="breadcrumb-item">
+                        <a href="<?php echo $f->url('/activities') ?>">
+                            Aktivitas
+                        </a>
+                    </li>
+                    <li class="breadcrumb-item active"><?php echo $activity->getTitle() ?></li>
+                </ol>
+            </nav>
+        </header>
+        <main class="flex-grow-1">
+            <div class="container pb-5">
+                <?php
+                $category = $activity->getCategory();
+                if ($category) {
+                ?>
+                    <span class="badge badge-default px-2 mr-2">
+                        <i class="<?php echo $category->getIconName() ?> pr-2" aria-hidden="true"></i> <?php echo $category->getName() ?>
+                    </span>
+                <?php } ?>
+                <h2 class="h2-responsive text-center mb-5"><?php echo $activity->getTitle() ?></h2>
+                <p>oleh <a><strong><?php echo $activity->getCreatorId() ?></strong></a>, <?php echo $activity->getCreationDate() ?></p>
+                <figure class="figure">
+                    <img src="<?php echo $f->url('/data/public/images/activities/' . ((($activity->getId() - 1) % 5) + 1) . '.jpg') ?>" class="figure-img img-fluid z-depth-1" alt="...">
+                </figure>
+                <div class="mb-4"><?php echo $activity->getContent() ?></div>
+            </div>
+        </main>
+        <?php include('./web/template/footer.php') ?>
+        <?php include('./web/template/scripts.php') ?>
+    </body>
+
 </html>
